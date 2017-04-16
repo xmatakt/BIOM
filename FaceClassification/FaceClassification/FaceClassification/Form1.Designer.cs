ab964384712bbf569f33b74ddfd34b75e74e9cbc
xmatakt <timotej.matak@gmail.com>
2017-04-16 16:45:10 +0200

    pred zacatim SVM

--- FaceClassification/FaceClassification/FaceClassification/Form1.Designer.cs
+++ FaceClassification/FaceClassification/FaceClassification/Form1.Designer.cs
@@ -28,20 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chooseFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pCAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pcaComputeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.selectedFolderLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.pCAToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(732, 28);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.chooseFolderToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // chooseFolderToolStripMenuItem
+            // 
+            this.chooseFolderToolStripMenuItem.Name = "chooseFolderToolStripMenuItem";
+            this.chooseFolderToolStripMenuItem.Size = new System.Drawing.Size(177, 26);
+            this.chooseFolderToolStripMenuItem.Text = "Choose folder";
+            this.chooseFolderToolStripMenuItem.Click += new System.EventHandler(this.chooseFolderToolStripMenuItem_Click);
+            // 
+            // pCAToolStripMenuItem
+            // 
+            this.pCAToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pcaComputeToolStripMenuItem});
+            this.pCAToolStripMenuItem.Name = "pCAToolStripMenuItem";
+            this.pCAToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.pCAToolStripMenuItem.Text = "Datasets";
+            // 
+            // pcaComputeToolStripMenuItem
+            // 
+            this.pcaComputeToolStripMenuItem.Name = "pcaComputeToolStripMenuItem";
+            this.pcaComputeToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.pcaComputeToolStripMenuItem.Text = "Create";
+            this.pcaComputeToolStripMenuItem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.pcaComputeToolStripMenuItem.Click += new System.EventHandler(this.pcaComputeToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectedFolderLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 436);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(732, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // selectedFolderLabel
+            // 
+            this.selectedFolderLabel.Name = "selectedFolderLabel";
+            this.selectedFolderLabel.Size = new System.Drawing.Size(0, 17);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(561, 458);
+            this.ClientSize = new System.Drawing.Size(732, 458);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chooseFolderToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel selectedFolderLabel;
+        private System.Windows.Forms.ToolStripMenuItem pCAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pcaComputeToolStripMenuItem;
     }
 }
 
--- FaceClassification/FaceClassification/FaceClassification/Form1.cs
+++ FaceClassification/FaceClassification/FaceClassification/Form1.cs
@@ -7,14 +7,174 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
+using Emgu.CV.Structure;
 
 namespace FaceClassification
 {
     public partial class Form1 : Form
     {
+        private const int maxComponents = 20;
+        private string selectedFolder = "";
+        private bool canContinue;
+        private bool debug = true;
+        private Dictionary<int, List<Image<Gray, byte>>> originalImages;
+        private Dictionary<int, List<Image<Gray, byte>>> pcaImages;
+        private DataSet trainingSet;
+        private DataSet testSet;
         public Form1()
         {
+            trainingSet = new DataSet();
+            testSet = new DataSet();
+
             InitializeComponent();
+
+            if (debug)
+            {
+                selectedFolder =
+                    @"C:\Users\Timotej\Google Drive\Skola\API\semester_2\Biometria\cvicenia\zadanie_4\att_faces";
+                selectedFolderLabel.Text =
+                    @"C:\Users\Timotej\Google Drive\Skola\API\semester_2\Biometria\cvicenia\zadanie_4\att_faces";
+                canContinue = true;
+            }
+        }
+
+        private void chooseFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedFolder = folderBrowserDialog.SelectedPath;
+                selectedFolderLabel.Text = selectedFolder;
+                canContinue = true;
+            }
+        }
+
+        private void pcaComputeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!canContinue)
+            {
+                MessageBox.Show("You have to choose the folder first (File -> Choose folder)");
+                return;
+            }
+
+            if (!IsFolderValid())
+            {
+                MessageBox.Show("Choosen folder is not in the right format!");
+                return;
+            }
+
+            LoadImages();
+            CreatePcaImages();
+            GenerateDataSets();
+        }
+
+        private bool IsFolderValid()
+        {
+            var subDirectories = new DirectoryInfo(selectedFolder).GetDirectories();
+            if (subDirectories.Length == 0)
+                return false;
+
+            foreach (var subDirectory in subDirectories)
+            {
+                var subdirectoryContent = subDirectory.GetFiles().Where(x => x.Extension == ".png");
+                if (!subdirectoryContent.Any())
+                    return false;
+            }
+
+            return true;
+        }
+
+        private void LoadImages()
+        {
+            originalImages = new Dictionary<int, List<Image<Gray, byte>>>();
+            pcaImages = new Dictionary<int, List<Image<Gray, byte>>>();
+            var subDirectories = new DirectoryInfo(selectedFolder).GetDirectories();
+            var index = 1;
+            foreach (var subDirectory in subDirectories)
+            {
+                var files = subDirectory.GetFiles();
+                foreach (var file in files.Where(x => x.Extension == ".png").Select(x => x.FullName))
+                {
+                    var img = Image.FromFile(file);
+                    var image = new Image<Gray, byte>(new Bitmap(img));
+                    if (originalImages.ContainsKey(index))
+                        originalImages[index].Add(image);
+                    else
+                    {
+                        originalImages.Add(index, new List<Image<Gray, byte>>());
+                        originalImages[index].Add(image);
+                    }
+                }
+                index++;
+            }
+        }
+
+        private void CreatePcaImages()
+        {
+            foreach (var keyPairValue in originalImages)
+            {
+                var label = keyPairValue.Key;
+                foreach (var image in keyPairValue.Value)
+                {
+                    var meanMatrix = new Mat();
+                    var eigenVectors = new Mat();
+                    var projection = new Mat();
+                    CvInvoke.PCACompute(image, meanMatrix, eigenVectors, maxComponents);
+                    CvInvoke.PCAProject(image, meanMatrix, eigenVectors, projection);
+                    var projectedImage = projection.ToImage<Gray, byte>();
+
+                    if (pcaImages.ContainsKey(label))
+                        pcaImages[label].Add(projectedImage);
+                    else
+                    {
+                        pcaImages.Add(label, new List<Image<Gray, byte>>());
+                        pcaImages[label].Add(projectedImage);
+                    }
+                    //iba na kontrolu funkcnosti
+                    //var backprojectedImage = new Mat();
+                    //CvInvoke.PCABackProject(projectedImage, meanMatrix, eigenVectors, backprojectedImage);
+                    //projectedImage.ToImage<Gray,byte>().ToBitmap().Save("tmp.png");
+                    //backprojectedImage.ToImage<Gray, byte>().ToBitmap().Save("backproject.png");
+                    //Emgu.CV.CvInvoke.Mahalanobis(, , );
+                    //CvInvoke.CalcCovarMatrix(,,,CovarMethod.Cols,DepthType.Cv64F);
+                } 
+            }
+        }
+
+        private void GenerateDataSets()
+        {
+            foreach (var keyPairValue in pcaImages)
+            {
+                var label = keyPairValue.Key;
+                var images = keyPairValue.Value;
+                // trenovacie data budu obsahovat 60% prvkov
+                var randomIndexes = GenerateRandomIndexes((int)(images.Count * 0.6), images.Count);
+                for (int i = 0; i < images.Count; i++)
+                {
+                    if (randomIndexes.Contains(i))
+                        trainingSet.Add(images[i], label);
+                    else
+                        testSet.Add(images[i], label);
+                }
+            }
+        }
+
+        private int[] GenerateRandomIndexes(int retArrLength, int maxIndexCount)
+        {
+            var result = new List<int>();
+            var rand = new Random();
+
+            while (result.Count != retArrLength)
+            {
+                var randIndex = rand.Next(0, maxIndexCount);
+                if (!result.Contains(randIndex))
+                    result.Add(randIndex);
+            }
+
+            return result.ToArray();
         }
     }
 }
